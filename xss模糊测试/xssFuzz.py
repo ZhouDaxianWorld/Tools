6e7b073eb80e0db56a55886cbf5b2589a4da3170
ZhouDaxian <ZhouDaxian.World@gmail.com>
2019-09-16 18:01:42 +0800

    commit

--- xss模糊测试/xssFuzz.py
+++ xss模糊测试/xssFuzz.py
@@ -6,6 +6,13 @@ import json
 import payload
 import utils.log as log
 
+"""
+根据经验所得，可以测试以下几种payload：
+1. 无任何转码的payload
+2. 使用url编码转换的payload
+3. 使用两次url编码的payload
+4. 对于href="可控变量"可使用unicode字符的payload
+"""
 logger = log.getLogger(__name__)
 
 def xssFuzz(url, method, params, data, headers, timeout=30):
